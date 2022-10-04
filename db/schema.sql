0e09c362ec29bb996c2cba1176edc7c36421f424
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-04 14:28:37 -0700

    create roles table

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,20 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
--- creating department table
+
 CREATE TABLE department (
   id INT NOT NULL PRIMARY KEY,
   name VARCHAR(30) NOT NULL
-)
+);
+
+
+CREATE TABLE roles (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+);
+
