87bc9cf24d91d5a9de1af3d50dce8a57ab3eb67b
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-04 14:17:51 -0700

    create department table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
 USE company_db;
+
+-- creating department table
+CREATE TABLE department (
+  id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+)
