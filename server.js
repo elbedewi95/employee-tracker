52135e7c646120fdd9fe83d5052ab19c5c5330c6
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-04 14:12:25 -0700

    create db connection

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const inquirer = require("inquirer");
-const db = require ("mysql2");
+const mysql = require ("mysql2");
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'abc123...',
+      database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+  );
