be1d8f27a728e9d4622fc7fb338528852b16a3da
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-04 19:07:49 -0700

    Prompts

--- server.js
+++ server.js
@@ -5,11 +5,69 @@ const mysql = require ("mysql2");
 const db = mysql.createConnection(
     {
       host: 'localhost',
+      port: 3306,
       // MySQL username,
       user: 'root',
       // MySQL password
       password: 'abc123...',
       database: 'company_db'
     },
-    console.log(`Connected to the company_db database.`)
+    console.log(`Connected to the company_db database.`),
+    promptOptions()
+
   );
+  
+
+  // First Prompt
+function promptOptions() {
+
+    inquirer.prompt({
+        type: "list",
+        name: "options",
+        message: "Select an Option",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "End"]
+      })
+      .then(function ({ options }) {
+        switch (options) {
+          case "View all departments":
+            //viewDepartments();
+            break;
+  
+          case "View all roles":
+           // viewRoles();
+            break;
+        
+          case "View all employees":
+            //viewEmployees();
+            break;
+  
+          case "Add a department":
+           // addDepartment();
+            break;
+
+          case "Add a Role":
+           // addRole();
+            break;
+          case "Add an Employee":
+           // addEmployee();
+            break;
+
+          case "Update an employee role":
+           // updateEmployeeRole();
+            break;
+  
+  
+          case "End":
+            db.end();
+            break;
+        }
+      });
+  }
